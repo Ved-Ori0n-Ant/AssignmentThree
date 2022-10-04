01dc398270dc758e35b0fb695b37d49ce1205062
Ved-Ori0n-Ant <190310116013@lecmorbi.gujgov.edu.in>
2022-10-04 16:41:41 +0530

    Added simple bottom-tab-navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ListingComponent from './src/screens/homeScreen';
 import FormComponent from './src/screens/firstScreen';
+//import Ionic from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
-const Stack = createNativeStackNavigator();
+//const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return(
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //     <Stack.Screen name = "homeScreen" component = {ListingComponent} />
+    //     <Stack.Screen name = "formScreen" component = {FormComponent} />
+    //     {/* <Stack.Screen name = "displayScreen" component = {DisplayComponent} /> */}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "homeScreen" component = {ListingComponent} />
-        <Stack.Screen name = "formScreen" component = {FormComponent} />
-        {/* <Stack.Screen name = "displayScreen" component = {DisplayComponent} /> */}
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name = "homeScreen" component = {ListingComponent} />
+        <Tab.Screen name = "formScreen" component = {FormComponent} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
